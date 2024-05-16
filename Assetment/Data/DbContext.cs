7ef43ea8b86499c412f16a31d8e5bcc0d59aa3ad
programmertsh <User@MSI>
2024-05-16 11:57:18 +0800

    Add project files.

---
+++ Assetment/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Assetment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assetment.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Assetment/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assetment.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Mail { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string Skillsets { get; set; }
+        public string Hobby { get; set; }
+    }
+}
